6532b340d5269fbaf752131770f03c1ddb01af0a
柳鑫豪 <125435595+liuxinhao18@users.noreply.github.com>
2023-03-17 10:48:25 +0800

    Create 001.cs

---
+++ c#/001.cs
@@ -0,0 +1,30 @@
+using System; //引入命名空间 已废弃Systen
+
+namespace _001_开始
+{
+    //这个是Program类
+    internal class Program
+    {
+        //这个是main方法
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); //这个是输出语句
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("你好世界");
+            Console.Write("hai 我不会换行");
+            Console.WriteLine("hai 我会换行");//指执行完这句话自动换行
+            Console.WriteLine("hai \t我是制表符（tab四个空格）");
+            Console.WriteLine("hai \n我是换行符");
+
+
+            /* asdasda
+                 dasdsd
+                 dadsda*/
+        }
+    }
+}
+
+namespace name
+{
+
+}
